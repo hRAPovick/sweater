4ebcba2bbc389f5ed0d4e1827d270f561c75ab73
hRAPovick <qsub.prod@gmail.com>
2021-05-10 19:12:19 +0300

    video 9: 4:00

--- src/main/java/com/sweater/service/UserService.java
+++ src/main/java/com/sweater/service/UserService.java
@@ -1,11 +1,17 @@
 package com.sweater.service;
 
+import com.sweater.domain.Role;
+import com.sweater.domain.User;
 import com.sweater.repository.UserRepository;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.UUID;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -15,5 +21,23 @@ public class UserService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username);
+    }
+
+    public boolean addUser(User user) {
+        User userFromDb = userRepository.findByUsername(user.getUsername());
+
+        if (userFromDb != null) {
+            return false;
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        user.setActivationCode(UUID.randomUUID().toString());
+        userRepository.save(user);
+
+        if (!StringUtils.isBlank(user.getEmail())){
+
+        }
+        return true;
     }
 }
