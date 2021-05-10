35aae47d30c3da2151de4a73f5e669761629d3d6
hRAPovick <qsub.prod@gmail.com>
2021-05-10 12:52:24 +0300

    userEdit

--- src/main/java/com/sweater/controller/UserController.java
+++ src/main/java/com/sweater/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.sweater.controller;
 
+import com.sweater.domain.Role;
+import com.sweater.domain.User;
 import com.sweater.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,5 +20,12 @@ public class UserController {
     public String userList(Model model) {
         model.addAttribute("users", userRepository.findAll());
         return "userList";
+    }
+
+    @GetMapping("{user}")
+    public String userEditForm(@PathVariable User user, Model model) {
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        return "userEdit";
     }
 }
