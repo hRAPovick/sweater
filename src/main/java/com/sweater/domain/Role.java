213092c32bf9d52457d582c94f4fb21193042a97
hRAPovick <qsub.prod@gmail.com>
2021-05-11 15:23:16 +0300

    Migration, profile

--- src/main/java/com/sweater/domain/Role.java
+++ src/main/java/com/sweater/domain/Role.java
@@ -2,7 +2,7 @@ package com.sweater.domain;
 
 import org.springframework.security.core.GrantedAuthority;
 
-public enum  Role implements GrantedAuthority {
+public enum Role implements GrantedAuthority {
     USER, ADMIN;
 
     @Override
