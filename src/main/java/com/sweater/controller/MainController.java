e30a3dc92f0cf2c2f8802e3e83dafa3b3e3b83a3
hRAPovick <qsub.prod@gmail.com>
2021-05-21 12:36:17 +0300

    usersMessages and edit it

--- src/main/java/com/sweater/controller/MainController.java
+++ src/main/java/com/sweater/controller/MainController.java
@@ -2,7 +2,8 @@ package com.sweater.controller;
 
 import com.sweater.domain.Message;
 import com.sweater.domain.User;
-import com.sweater.repository.MessageRepository;
+import com.sweater.service.MessageService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,15 +19,18 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.validation.Valid;
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 import java.util.UUID;
 
 @Controller
 public class MainController {
 
     @Autowired
-    private MessageRepository messageRepository;
+    private MessageService messageService;
 
     @Value("${upload.path}")
     private String uploadPath;
@@ -41,9 +46,9 @@ public class MainController {
         Iterable<Message> messages;
 
         if (filter != null && !filter.isEmpty()) {
-            messages = messageRepository.findByTag(filter);
+            messages = messageService.findByTag(filter);
         } else {
-            messages = messageRepository.findAll();
+            messages = messageService.findAll();
         }
 
         model.addAttribute("messages", messages);
@@ -58,37 +63,85 @@ public class MainController {
             BindingResult bindingResult,
             Model model,
             @RequestParam("file") MultipartFile file
-    ) throws IOException {
+    ) throws IOException, URISyntaxException {
         message.setAuthor(user);
-
         if (bindingResult.hasErrors()) {
             Map<String, String> errorsMap = ControllerUtils.getErrors(bindingResult);
-
             model.mergeAttributes(errorsMap);
             model.addAttribute("message", message);
         } else {
-
-            if (file != null && !Objects.requireNonNull(file.getOriginalFilename()).isEmpty()) {
-                File uploadDir = new File(System.getProperty("user.dir") + "\\src\\" + uploadPath);
-                if (!uploadDir.exists()) {
-                    uploadDir.mkdir();
-                }
-
-                String uuidFile = UUID.randomUUID().toString();
-                String resultFilename = uuidFile + "." + file.getOriginalFilename();
-
-                file.transferTo(new File(uploadDir.getAbsolutePath() + "/" + resultFilename));
-
-                message.setFilename(resultFilename);
-            }
+            saveFile(message, file);
             model.addAttribute("message", null);
-
-            messageRepository.save(message);
-
+            messageService.save(message);
         }
-        Iterable<Message> messages = messageRepository.findAll();
+        Iterable<Message> messages = messageService.findAll();
         model.addAttribute("messages", messages);
         return "main";
+    }
+
+    private void saveFile(
+            @Valid Message message,
+            @RequestParam("file") MultipartFile file) throws IOException, URISyntaxException {
+        if (file != null && !Objects.requireNonNull(file.getOriginalFilename()).isEmpty()) {
+            URL dirURL = MainController.class.getClassLoader().getResource(uploadPath);
+            File uploadDir = null;
+            if (dirURL != null && dirURL.getProtocol().equals("file")) {
+                /* A file path: easy enough */
+                uploadDir = new File(dirURL.toURI());
+            } else {
+                /*
+                 * In case of a jar file, we can't actually find a directory.
+                 * Have to assume the same jar as clazz.
+                 */
+                uploadDir = new File("/tmp/" + uploadPath);
+            }
+            if (!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+            file.transferTo(new File(uploadDir + "/" + resultFilename));
+            message.setFilename(resultFilename);
+        }
+    }
+
+    @GetMapping("user-messages/{user}")
+    public String userMessages (
+            @AuthenticationPrincipal User currentUser,
+            @PathVariable User user,
+            Model model,
+            @RequestParam(required = false) Message message
+    ) {
+        Iterable<Message> messages = messageService.findByAuthor(user);
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("message", message);
+        model.addAttribute("isCurrentUser", (currentUser.getId()).equals(user.getId()));
+
+        return "userMessages";
+    }
+
+    @PostMapping("user-messages/{user}")
+    public String updateMessage(
+            @AuthenticationPrincipal User currentUser,
+            @PathVariable Long user,
+            @RequestParam("id") Message message,
+            @RequestParam("text") String text,
+            @RequestParam("tag") String tag,
+            @RequestParam("file") MultipartFile file
+    ) throws IOException, URISyntaxException {
+        if (user.equals(currentUser.getId())) {
+            if (!StringUtils.isBlank(text)) {
+                message.setText(text);
+            }
+            if (!StringUtils.isBlank(tag)) {
+                message.setTag(tag);
+            }
+            saveFile(message, file);
+            messageService.save(message);
+        }
+
+        return "redirect:/user-messages/" + user;
     }
 
 }
